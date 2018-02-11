9cc138648c97758192e1ac2a9773c295772e0226
Ganesh Hubale <ganeshhubale03@gmail.com>
2018-02-11 18:14:07 +0530

    version update and READ.md converted to README.rst

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
+import os
 from setuptools import setup, find_packages
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(  
     name="sticks",
     packages=find_packages(),
-    version='0.0.1',
+    version='0.0.4',
     description="Simple game application.",
-    long_description="long description.",
+    long_description=read('README.rst'),
     author="ganeshhubale.",
     author_email="ganeshhubale03@gmail.com",
     url="https://github.com/ganeshhubale/game_of_sticks",
