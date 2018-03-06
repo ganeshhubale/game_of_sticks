a1e7093fe393a379db7f630d6f95c1783a1a1ad0
Ganesh Hubale <ganeshhubale03@gmail.com>
2018-03-06 17:53:54 +0530

    release 0.0.9

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.rst") as readme_file:
 setup(  
     name="sticks",
     packages=find_packages(),
-    version='0.0.8',
+    version='0.0.9',
     description="Simple game application.",
     long_description=long_description,
     author="ganeshhubale.",
