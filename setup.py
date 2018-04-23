2101003e4cce39b09ff53f4124cc3c1bc6ec9286
Ganesh Hubale <ganeshhubale03@gmail.com>
2018-04-23 12:01:26 +0530

    0.13

--- setup.py
+++ setup.py
@@ -1,25 +1,14 @@
-import os
-from setuptools import setup, find_packages
 from distutils.core import setup
-
-with open("README.rst") as readme_file:
-    long_description = readme_file.read()
-
-setup(  
-    name="sticks",
-    packages=find_packages(),
-    version='0.0.13',
-    description="Simple game application.",
-    long_description=long_description,
-    author="ganeshhubale.",
-    author_email="ganeshhubale03@gmail.com",
-    url="https://github.com/ganeshhubale/game_of_sticks",
-    license="MIT",
-    py_modules=['sticks.__init__'],
-    namespace_packages=[],
-  #  entry_points={
-   #     'console_scripts': [
-    #        'sticks = sticks.__init__:main',
-     #   ],
-    },
+setup(
+  name = 'sticks',
+  packages = ['sticks'], # this must be the same as the name above
+  version = '0.13',
+  description = 'A random test lib',
+  author = 'ganesh',
+  author_email = 'ganeshhubale@gmail.com',
+  url = 'https://github.com/ganeshhubale/game_of_sticks', 
+ # download_url = 'https://github.com/peterldowns/mypackage/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['testing', 'logging', 'sticks'], # arbitrary keywords
+  classifiers = [],
 )
+
