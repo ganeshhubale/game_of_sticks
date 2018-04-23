30809408cfc607744320a75cf8d7b98b67064546
Ganesh Hubale <ganeshhubale03@gmail.com>
2018-04-23 12:07:36 +0530

    emntry point

--- setup.py
+++ setup.py
@@ -10,5 +10,10 @@ setup(
  # download_url = 'https://github.com/peterldowns/mypackage/archive/0.1.tar.gz', # I'll explain this in a second
   keywords = ['testing', 'logging', 'sticks'], # arbitrary keywords
   classifiers = [],
+     entry_points={
+         'console_scripts': [
+             'sticks = sticks.__init__:main',
+],
+         },
 )
 
